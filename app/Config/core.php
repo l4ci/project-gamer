ecb1ecd4945d4c5155f1e96342741954b061c34b
Volker Otto <pim.volker@gmail.com>
2014-08-03 14:45:45 +0200

    changed salt and cipher seed

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'VYq095b0qyJfjG6z2guVlSubWnvn832xG0Fga415i');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '83629309657132542497453683981');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
